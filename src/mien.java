56b7775cb92508d4e6a2881b1b8cbba198d20641
zeiger2 <zverevart@inbox.ru>
2024-09-08 12:50:31 +0300

    test

---
+++ src/mien.java
@@ -0,0 +1,10 @@
+public class mien {
+    public static void main(String[] args) {
+        int A[]={5,3,2,1,4,0};
+        int temp;
+        for(int i=0; i<A.length; i++){
+            for(int j=i+1; )
+        }
+
+    }
+}
