650827cc7bb5554017f2c282deac16a2685c2ce5
zeiger2 <zverevart@inbox.ru>
2024-09-08 13:06:40 +0300

    0.1

--- src/mien.java
+++ src/mien.java
@@ -1,19 +1,5 @@
 public class mien {
     public static void main(String[] args) {
-        int[] A ={5,3,2,1,4,0,-4};
-        int temp;
-        for(int i=0; i<A.length; i++){
-            for(int j=i+1; j<A.length; j++){
-                if(A[j]<A[i]){
-                    temp=A[i];
-                    A[i]=A[j];
-                    A[j]=temp;
-                }
-            }
-        }
-        System.out.println("Sorted Array:");
-        for(int i=0; i<A.length; i++){
-            System.out.print(A[i]+" ");
-        }
+
     }
 }
